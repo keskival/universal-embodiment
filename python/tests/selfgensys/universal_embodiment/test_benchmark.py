3cb94557f7c21cb54634bdd20df284ad12f4e3dc
Tero Keski-Valkama <tero.keskivalkama@gmail.com>
2024-10-27 12:02:52 +0100

    Test shows a bug.

--- python/tests/selfgensys/universal_embodiment/test_benchmark.py
+++ python/tests/selfgensys/universal_embodiment/test_benchmark.py
@@ -1,4 +1,19 @@
 from selfgensys.universal_embodiment.benchmark import get_best_choice, Rules, World
 
 def test_get_best_choice():
-    pass
+    rules = Rules(num_agent_types=1, num_prey_types=3, size_world=10)
+    world = World(rules=rules, num_prey=3)
+    # Let's make the right prey of negative value, and the left prey as well
+    # but the left-most prey of a higher positive value.
+    world.prey_positions = [3, 4, 6]
+    # The correct action is to take both preys to the left and leave the prey
+    # on the right.
+    world.agent_type = 0
+    world.prey_types = [0, 1, 2]
+    rules.preferences = [[5.0, -1,0, -1.0]]
+    move = world.get_optimal_move()
+    assert(-1 == move)
+    move = world.get_optimal_move()
+    assert(-1 == move)
+    move = world.get_optimal_move()
+    assert(None == move)
